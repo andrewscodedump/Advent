3eb8ae550bf6d688b51d1224c214e15a705025f7
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2021-12-01 08:13:33 +0000

    Day1

---
+++ 2021/Done/Days01-05/Day01.cs
@@ -0,0 +1,19 @@
+﻿namespace Advent2021;
+
+public partial class Day01 : Advent.Day
+{
+    public override void DoWork()
+    {
+        int curr, prev = int.MaxValue, increases = 0;
+
+        for (int i=0; i<InputSplitInt.Length; i++)
+        {
+            if (WhichPart == 2 && i > InputSplitInt.Length - 3) break;
+            curr = WhichPart == 1 ? InputSplitInt[i] : InputSplitInt[i] + InputSplitInt[i + 1] + InputSplitInt[i + 2];
+            if (curr > prev) increases++;
+            prev = curr;
+        }
+
+        Output = increases.ToString();
+    }
+}
