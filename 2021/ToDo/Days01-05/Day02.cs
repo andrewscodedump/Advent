701594f762677d89aaf27f30f375e93a36859782
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2021-12-02 08:31:13 +0000

    Day 2

--- 2021/ToDo/Days01-05/Day02.cs
+++ 2021/ToDo/Days01-05/Day02.cs
@@ -4,11 +4,13 @@ public partial class Day02 : Advent.Day
 {
     public override void DoWork()
     {
-        #region Setup Variables and Parse Inputs
-
-        #endregion Setup Variables and Parse Inputs
-
-        // Add code here
-        Output = "OutputVariable".ToString();
+        Dictionary<string, (int x, int y)> dirns = new() { { "down", (0, 1) }, { "up", (0, -1) }, { "back", (-1, 0) }, { "forward", (1, 0) } };
+        int x = 0, y = 0, aim = 0;
+        foreach (string instr in InputSplit)
+        {
+            string dirn = instr.Split(' ')[0]; int dist = int.Parse(instr.Split(' ')[1]); aim += dirns[dirn].y * dist;
+            (x, y) = (x + (dirns[dirn].x * dist), y + (WhichPart == 1 ? (dirns[dirn].y * dist) : (dirns[dirn].x * dist * aim)));
+        }
+        Output = (x * y).ToString();
     }
 }
