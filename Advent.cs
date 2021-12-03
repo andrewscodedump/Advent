a620af3c17183c7d3b4dda7a866e20ab292f2d72
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2021-12-03 12:52:32 +0000

    Day03

--- Advent.cs
+++ Advent.cs
@@ -8,7 +8,7 @@ namespace Advent;
 public partial class AdventOfCode : Form
 {
     #endregion Preamble
-    private readonly int defaultDay = 2;
+    private readonly int defaultDay = 3;
     private readonly int defaultPuzzle = 1;
     private readonly bool defaultTestMode = true;
     private readonly int defaultYear = 2021;
