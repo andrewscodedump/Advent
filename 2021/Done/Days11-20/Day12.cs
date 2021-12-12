c459e6ef41d05f9a4b4e60ac6c6aec4b6efcf797
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2021-12-12 12:45:03 +0000

    Small changes, for tiny marginal gains.

--- 2021/Done/Days11-20/Day12.cs
+++ 2021/Done/Days11-20/Day12.cs
@@ -4,25 +4,27 @@ public partial class Day12 : Advent.Day
 {
     public override void DoWork()
     {
-        Dictionary<string, List<string>> options = new();
+        Dictionary<string, HashSet<string>> options = new();
         Queue<(string, List<string>, HashSet<string>, bool)> explore = new();
         List<List<string>> paths = new();
 
-        foreach(string input in InputSplit)
+        foreach (string input in InputSplit)
         {
             string start = input.Split('-')[0], end = input.Split('-')[1];
+            if (start == "start" || start == "end") start = start.ToUpper();
+            if (end == "start" || end == "end") end = end.ToUpper();
             if (!options.ContainsKey(start)) options[start] = new();
             if (!options.ContainsKey(end)) options[end] = new();
-            if (start != "end" && end != "start") options[start].Add(end);
-            if (start != "start" && end != "end") options[end].Add(start);
+            if (start != "END" && end != "START") options[start].Add(end);
+            if (start != "START" && end != "END") options[end].Add(start);
         }
-        explore.Enqueue(("start", new(), new(), false));
+        explore.Enqueue(("START", new(), new(), false));
 
         do
         {
             (string pos, List<string> path, HashSet<string> visited, bool twoVisits) = explore.Dequeue();
             path.Add(pos);
-            if (pos == "end")
+            if (pos == "END")
             {
                 paths.Add(path);
                 continue;
@@ -30,7 +32,7 @@ public partial class Day12 : Advent.Day
             visited.Add(pos);
             foreach (string nextPos in options[pos])
             {
-                bool nextSmall = nextPos[0] > 96 && nextPos!= "end", nextTwo = twoVisits;
+                bool nextSmall = nextPos[0] > 96, nextTwo = twoVisits;
                 if (visited.Contains(nextPos) && nextSmall)
                 {
                     if (WhichPart == 1 || twoVisits) continue;
@@ -40,7 +42,7 @@ public partial class Day12 : Advent.Day
             }
 
         } while (explore.Count > 0);
-        
+
         Output = paths.Count.ToString();
     }
 }
