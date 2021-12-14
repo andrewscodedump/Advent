4d01136823c229a1f2b1652a8fb578717a25392c
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2021-12-14 01:24:59 +0000

    Day13

---
+++ 2021/Done/Days11-20/Day13.cs
@@ -0,0 +1,35 @@
+﻿namespace Advent2021;
+
+public partial class Day13 : Advent.Day
+{
+    public override void DoWork()
+    {
+        foreach (string line in InputSplit)
+        {
+            if (!line.StartsWith("fold"))
+                SimpleMap[(int.Parse(line.Split(',')[0]), int.Parse(line.Split(',')[1]))] = '█';
+            else
+            {
+                Dictionary<(int x, int y), char> afterFold = new(SimpleMap);
+                char direction = line[11];
+                int fold = int.Parse(line[13..]);
+                foreach ((int x, int y) in SimpleMap.Keys)
+                    if (direction == 'y' && y > fold)
+                    {
+                        afterFold[(x, (2 * fold) - y)] = '█';
+                        afterFold.Remove((x, y));
+                    }
+                    else if (direction == 'x' && x > fold)
+                    {
+                        afterFold[((2 * fold) - x, y)] = '█';
+                        afterFold.Remove((x, y));
+                    }
+                SimpleMap = new(afterFold);
+                if (WhichPart == 1) break;
+            }
+        }
+        if (WhichPart==2) DrawMap(false, false);
+
+        Output = WhichPart == 1 ? SimpleMap.Count.ToString() : "Result is in debug window";
+    }
+}
--- Program/Advent.cs
+++ Program/Advent.cs
@@ -8,7 +8,7 @@ namespace Advent;
 public partial class AdventOfCode : Form
 {
     #endregion Preamble
-    private readonly int defaultDay = 12;
+    private readonly int defaultDay = 13;
     private readonly int defaultPuzzle = 1;
     private readonly bool defaultTestMode = true;
     private readonly int defaultYear = 2021;
