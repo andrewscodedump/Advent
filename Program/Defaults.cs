fd647505a59f362c447b458e1a11c0319e86675d
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2022-12-01 13:13:14 +0000

    Split defaults to their own partial class.

---
+++ Program/Defaults.cs
@@ -0,0 +1,9 @@
+﻿namespace Advent;
+
+public partial class AdventOfCode
+{
+    private readonly int defaultDay = 1;
+    private readonly int defaultPuzzle = 1;
+    private readonly bool defaultTestMode = true;
+    private readonly int defaultYear = 2022;
+}
