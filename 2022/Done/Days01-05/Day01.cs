3ae9dd2691b45f3ae10897604fb183fc9d14f59d
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2022-12-01 13:18:18 +0000

    Day 1 Done

---
+++ 2022/Done/Days01-05/Day01.cs
@@ -0,0 +1,10 @@
+﻿namespace Advent2022;
+
+public partial class Day01 : Advent.Day
+{
+    public override void DoWork()
+    {
+        List<long> elfTotals = Input.Split(";;").Select(elf => elf.Split(';').Select(item => long.Parse(item)).Sum()).ToList();
+        Output = elfTotals.OrderByDescending(total => total).Take(WhichPart == 1 ? 1 : 3).Sum().ToString();
+    }
+}
