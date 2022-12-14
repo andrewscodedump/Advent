07866cf2b4d526d31a7b117717c0f42ab162c234
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2022-12-14 14:02:22 +0000

    Day 14 Done.

---
+++ 2022/Done/Days11-15/Day14.cs
@@ -0,0 +1,124 @@
+﻿namespace Advent2022;
+
+public partial class Day14 : Advent.Day
+{
+    public override void DoWork()
+    {
+        Dictionary<(int x, int y), char> map = DoScan(InputSplit);
+        int minX = map.Keys.Select(k => k.x).Min();
+        int maxX = map.Keys.Select(k => k.x).Max();
+        int maxY = map.Keys.Select(k => k.y).Max() + 2;
+        bool ended, debug = false;
+        int grainCount = 0;
+        if (debug) DrawMap(map);
+        do
+        {
+            ended = DropSand(map, minX, maxX, maxY);
+            if (debug) DrawMap(map);
+            if (!ended || Part2) grainCount++;
+        } while (!ended);
+
+        Output = grainCount.ToString();
+    }
+
+    private Dictionary<(int, int), char> DoScan(string[] input)
+    {
+        Dictionary<(int, int), char> map = new();
+        foreach (string s in input)
+        {
+            string[] vertices = s.Split(" -> ");
+            for (int v = 0; v < vertices.Length - 1; v++)
+            {
+                DrawLine(map, vertices[v], vertices[v + 1]);
+            }
+        }
+        return map;
+    }
+
+    private void DrawLine(Dictionary<(int, int), char> map, string from, string to)
+    {
+        int startX = int.Parse(from.Split(',')[0]), startY = int.Parse(from.Split(',')[1]);
+        int endX = int.Parse(to.Split(',')[0]), endY = int.Parse(to.Split(',')[1]);
+        (startX, endX) = (Math.Min(startX, endX), Math.Max(startX, endX));
+        (startY, endY) = (Math.Min(startY, endY), Math.Max(startY, endY));
+        if (startX == endX)
+        {
+            for (; startY <= endY; startY++)
+                map[(startX, startY)] = '#';
+        }
+        else if (startY == endY)
+        {
+            for (; startX <= endX; startX++)
+                map[(startX, startY)] = '#';
+        }
+        else
+        {
+            // Unexpected input
+            Debugger.Break();
+        }
+    }
+
+    private bool DropSand(Dictionary<(int, int), char> map, int minX, int maxX, int maxY)
+    {
+        (int x, int y) = (500, 0);
+        bool ended = false, stopped = false;
+        do
+        {
+            if (!map.TryGetValue((x, y + 1), out char below)) below = '.';
+            if (!map.TryGetValue((x - 1, y + 1), out char left)) left = '.';
+            if (!map.TryGetValue((x + 1, y + 1), out char right)) right = '.';
+            if (Part1)
+            {
+                if (x == minX) left = 'a';
+                if (x == maxX) right = 'a';
+            }
+            if (Part2 && y + 1 == maxY)
+            {
+                left = '#'; below = '#'; right = '#';
+            }
+            if (below == '.')
+                y++;
+            else if (left == '.')
+            {
+                y++;
+                x--;
+            }
+            else if (right == '.')
+            {
+                y++;
+                x++;
+            }
+            else if (left == 'a' || right == 'a')
+                ended = true;
+            else
+            {
+                map[(x, y)] = 'o';
+                stopped = true;
+                if (Part2 && y == 0) ended = true;
+            }
+            if (Part1 && y > maxY) ended = true;
+        } while (!ended && !stopped);
+
+        return ended;
+
+    }
+    private void DrawMap(Dictionary<(int, int), char> map)
+    {
+        int minX = map.Keys.Select(x => x.Item1).Min();
+        int maxX = map.Keys.Select(x => x.Item1).Max();
+        int maxY = map.Keys.Select(y => y.Item2).Max();
+        if (Part2) maxY += 1;
+        StringBuilder drawing = new();
+        for (int y = 0; y <= maxY; y++)
+        {
+            for (int x = minX; x <= maxX; x++)
+            {
+                if (!map.TryGetValue((x, y), out char item)) item = '.';
+                if (Part2 && y == maxY) item = '#';
+                drawing.Append(item);
+            }
+            drawing.AppendLine();
+        }
+        Debug.Print(drawing.ToString());
+    }
+}
--- Program/Defaults.cs
+++ Program/Defaults.cs
@@ -2,7 +2,7 @@
 
 public partial class AdventOfCode
 {
-    private readonly int defaultDay = 11;
+    private readonly int defaultDay = 14;
     private readonly int defaultPuzzle = 1;
     private readonly bool defaultTestMode = true;
     private readonly int defaultYear = 2022;
