168ed19fc64905b5fca9b3cc350f9bb04f740bbe
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2023-12-09 10:47:11 +0000

    2023 day 9 done

---
+++ 2023/Done/Days06-10/Day09.cs
@@ -0,0 +1,31 @@
+﻿namespace Advent2023;
+
+public partial class Day09 : Advent.Day
+{
+    public override void DoWork()
+    {
+        long part1 = 0, part2 = 0;
+
+        foreach (long[] history in InputNumbers)
+        {
+            long next = 0, prev = 0;
+            long[] nextLine = history, lastDigits = [], firstDigits = [];
+            do
+            {
+                firstDigits = [nextLine[0], .. firstDigits];
+                lastDigits = [nextLine[^1], .. lastDigits];
+                nextLine = [.. nextLine.Zip(nextLine.Skip(1), (prev, next) => next - prev)];
+            } while (nextLine.Any(h => h != 0));
+
+            for (int i = 0; i < lastDigits.Length; i++)
+            {
+                next += lastDigits[i];
+                prev = firstDigits[i] - prev;
+            }
+
+            part1 += next;
+            part2 += prev;
+        }
+        Output = (Part1 ? part1 : part2).ToString();
+    }
+}
