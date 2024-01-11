fce5d2a1d3a77a9e30c6838bb987f4061e281a18
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2024-01-11 23:33:54 +0000

    2023 Restore mysteriously lost day14 code

--- 2023/Done/Days11-15/Day14.cs
+++ 2023/Done/Days11-15/Day14.cs
@@ -3,8 +3,6 @@
 public partial class Day14 : Advent.Day
 {
     int width = 0, height = 0;
-    bool debug = false;
-
     public override void DoWork()
     {
         PopulateMapFromInputWithBorders('*', out width, out height);
@@ -21,19 +19,37 @@ public partial class Day14 : Advent.Day
         Output = totalScore.ToString();
     }
 
-    private int RunSpinCycle(int times)
+    private int RunSpinCycle(int targetCycles)
     {
-        bool repeat = false;
-        //HashSet<Dictionary<(int, int), char>> seenPositions = [];
-        HashSet<int> seenPositions = [];
+        bool repeatFound = false;
+        List<string> seenStates = [GetState(SimpleMap)];
         int result = 0, cycles = 0;
         do
         {
-            result = RunSpinCycle();
-            repeat = !seenPositions.Add(SimpleMap.GetHashCode());
             cycles++;
-        } while (!repeat && cycles < times);
+            result = RunSpinCycle();
+            string state = GetState(SimpleMap);
+            if (seenStates.Contains(state))
+            {
+                repeatFound = true;
+                int startOfRepeat = seenStates.IndexOf(state), endOfRepeat = cycles;
+                int targetSet = ((targetCycles - startOfRepeat) % (endOfRepeat - startOfRepeat)) + startOfRepeat;
+                result = GetWeightFromState(seenStates[targetSet]);
+            }
+            else
+                seenStates.Add(state);
+        } while (!repeatFound && cycles < targetCycles);
 
+        return result;
+    }
+
+    private int GetWeightFromState(string state)
+    {
+        int result = 0;
+        Debug.Print(state);
+        for (int i = 0; i < state.Length; i++)
+            if (state[i] == 'O')
+                result += height - (i / (width + 2)) + 1;
         return result;
     }
 
@@ -43,15 +59,27 @@ public partial class Day14 : Advent.Day
         MoveStones((-1, 0));
         MoveStones((0, 1));
         int result = MoveStones((1, 0));
-        if (debug) DrawMap(false, false);
         return result;
+    }
+
+    private static string GetState(Dictionary<(int x, int y), char> dict)
+    {
+        StringBuilder state = new();
+        foreach (KeyValuePair<(int, int), char> kvp in dict.OrderBy(d => (d.Key.y * 100) + d.Key.x))
+            state.Append(kvp.Value);
+        return state.ToString();
+
     }
     private int MoveStones((int dx, int dy) move)
     {
         int totalScore = 0;
-        for (int y = 0; y < height; y++)
+        int xStart = 0, xEnd = width, xOffset = 1;
+        int yStart = 0, yEnd = width, yOffset = 1;
+        if (move.dx == 1) { xStart = width - 1; xEnd = 1; xOffset = -1; }
+        if (move.dy == 1) { yStart = height - 1; yEnd = 1; yOffset = -1; }
+        for (int y = yStart; y * yOffset < yEnd; y += yOffset)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = xStart; x * xOffset < xEnd; x += xOffset)
             {
                 if (SimpleMap[(x, y)] != 'O') continue;
                 (int x, int y) currentPos = (x, y);
@@ -63,14 +91,8 @@ public partial class Day14 : Advent.Day
                     SimpleMap[nextPos] = 'O';
                     currentPos = nextPos;
                     nextPos = (currentPos.x + move.dx, currentPos.y + move.dy);
-                    //DrawMap(false, false);
                 }
                 totalScore += height - currentPos.y;
-                if (debug)
-                {
-                    Debug.Print(SimpleMap.GetHashCode().ToString());
-                    DrawMap(false, false);
-                }
             }
         }
         return totalScore;
