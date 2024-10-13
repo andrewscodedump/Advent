535c079da8c500d31f5b74e70783ead5ee826573
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2024-10-13 16:09:43 +0100

    Initial setup for 2024

---
+++ 2024/ToDo/Days01-05/Day01.cs
@@ -0,0 +1,14 @@
+﻿namespace Advent2024;
+
+public partial class Day01 : Advent.Day
+{
+    public override void DoWork()
+    {
+        #region Setup Variables and Parse Inputs
+
+        #endregion Setup Variables and Parse Inputs
+
+        // Add code here
+        Output = "OutputVariable".ToString();
+    }
+}
