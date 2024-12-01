d65a5e0dda321a3024b6b23d0b8a2c872f9a6bbf
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2024-12-01 23:48:06 +0000

    Day 01 Done

---
+++ 2024/Done/Days01-05/Day01.cs
@@ -0,0 +1,26 @@
+﻿namespace Advent2024;
+
+public partial class Day01 : Advent.Day
+{
+    public override void DoWork()
+    {
+        List<long> left = [], right = [];
+        long sum1 = 0, sum2 = 0;
+
+        foreach (long[] lists in InputNumbers)
+        {
+            left.Add(lists[0]);
+            right.Add(lists[1]);
+        }
+
+        left.Sort();
+        right.Sort();
+
+        for (int i = 0; i < left.Count; i++)
+        {
+            sum1 += Math.Abs(left[i] - right[i]);
+            sum2 += left[i] * right.Count(n => n == left[i]);
+        }
+        Output = (Part1 ? sum1 : sum2).ToString();
+    }
+}
