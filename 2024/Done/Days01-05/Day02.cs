7b7d2fe602ef8aeafdc1e2136f24759ac2eaabfe
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2024-12-02 13:50:18 +0000

    Day 02 Done

---
+++ 2024/Done/Days01-05/Day02.cs
@@ -0,0 +1,34 @@
+﻿namespace Advent2024;
+
+public partial class Day02 : Advent.Day
+{
+    public override void DoWork()
+    {
+        int totalSafe = 0;
+        foreach (long[] report in InputNumbers)
+        {
+            if (IsReportSafe(report))
+                totalSafe++;
+            else if (Part2)
+                for (int i = 0; i < report.Length; i++)
+                    if (IsReportSafe([.. report[..i], .. report[(i + 1)..]]))
+                    {
+                        totalSafe++;
+                        break;
+                    }
+        }
+        Output = totalSafe.ToString();
+    }
+
+    static bool IsReportSafe(long[] report)
+    {
+        long diff, prevDiff = 0;
+        for (int i = 0; i < report.Length - 1; i++)
+        {
+            diff = report[i] - report[i + 1];
+            if (diff == 0 || Math.Abs(diff) > 3 || diff * prevDiff < 0) return false;
+            prevDiff = diff;
+        }
+        return true;
+    }
+}
