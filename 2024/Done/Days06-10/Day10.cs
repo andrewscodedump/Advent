d7b0e1077af1e90dc2e4d104dc2f13fef2335df4
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2024-12-10 09:13:43 +0000

    2024 Day10 Done

---
+++ 2024/Done/Days06-10/Day10.cs
@@ -0,0 +1,29 @@
+﻿namespace Advent2024;
+
+public partial class Day10 : Advent.Day
+{
+    public override void DoWork()
+    {
+        int counter = 0;
+        Stack<((int, int), (int, int), int)> q = new();
+        HashSet<((int, int), (int, int))> visited = [];
+        PopulateMapFromInputWithBorders('.');
+
+        foreach ((int, int) pos in SimpleMap.Keys.Where(k => SimpleMap[k] == '0')) q.Push((pos, pos, '0'));
+        do
+        {
+            ((int x, int y) start, (int x, int y), int count) = q.Pop();
+            foreach ((int dx, int dy) in DirectNeighbours)
+            {
+                (int, int) nextPos = (x + dx, y + dy);
+                int nextVal = SimpleMap[nextPos];
+                if (nextVal != count + 1) continue;
+                if (Part1 && !visited.Add((start, nextPos))) continue;
+                if (nextVal == '9') { counter++; continue; }
+                q.Push((start, nextPos, nextVal));
+            }
+        } while (q.Count > 0);
+
+        Output = counter.ToString();
+    }
+}
