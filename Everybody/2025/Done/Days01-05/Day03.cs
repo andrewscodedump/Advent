618d73ecbb3d6f58b4842b5ca91e805bbb455ecf
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2025-11-05 23:59:46 +0000

    EC 2025 Day03 done.

---
+++ Everybody/2025/Done/Days01-05/Day03.cs
@@ -0,0 +1,20 @@
+﻿namespace Everybody2025;
+
+public class Day03 : Advent.Day
+{
+    public override void DoWork()
+    {
+        switch (WhichPart)
+        {
+            case 1:
+                Output = InputNumbers[0].Distinct().Sum().ToString();
+                break;
+            case 2:
+                Output = InputNumbers[0].Distinct().Order().Take(20).Sum().ToString();
+                break;
+            case 3:
+                Output = InputNumbers[0].GroupBy(n => n).Max(g => g.Count()).ToString();
+                break;
+        }
+    }
+}
