4746972d20851e15589901fada2ab25018fca360
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2025-11-11 19:34:57 +0000

    Changed project to .net 10.  Changed Day06 to use span

--- Everybody/2025/Done/Days06-10/Day06.cs
+++ Everybody/2025/Done/Days06-10/Day06.cs
@@ -4,20 +4,18 @@ public class Day06 : Advent.Day
 {
     public override void DoWork()
     {
-        int pairs = 0;
-        int repeats = WhichPart == 3 ? 1000 : 1, limit = 1000;
-        string line = string.Concat(Enumerable.Repeat(Input, repeats));
+        int pairs = 0, repeats = WhichPart == 3 ? 1000 : 1, limit = 1000;
+        ReadOnlySpan<char> line = new(string.Concat(Enumerable.Repeat(Input, repeats)).ToCharArray());
         for (int i = 1; i < line.Length; i++)
         {
-            if (line[i] < 97) continue;
-            if (WhichPart == 1 && Input[i] != 'a') continue;
+            if (line[i] < 'a' || (WhichPart == 1 && Input[i] != 'a')) continue;
             int start = 0, end = i;
             if (WhichPart == 3)
             {
                 start = Math.Max(0, i - limit);
                 end = Math.Min(line.Length, i + limit + 1);
             }
-            pairs += line[start..end].ToCharArray().Count(c => c == line[i] - 32);
+            pairs += line[start..end].Count((char)(line[i] - 32));
         }
         Output = pairs.ToString();
     }
