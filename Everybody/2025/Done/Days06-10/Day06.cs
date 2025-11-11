bb64fcd826d6f658ad48a0b45fec8234926ed34f
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2025-11-11 00:11:48 +0000

    EC2025 Day06 Done (poorly)

---
+++ Everybody/2025/Done/Days06-10/Day06.cs
@@ -0,0 +1,24 @@
+﻿namespace Everybody2025;
+
+public class Day06 : Advent.Day
+{
+    public override void DoWork()
+    {
+        int pairs = 0;
+        int repeats = WhichPart == 3 ? 1000 : 1, limit = 1000;
+        string line = string.Concat(Enumerable.Repeat(Input, repeats));
+        for (int i = 1; i < line.Length; i++)
+        {
+            if (line[i] < 97) continue;
+            if (WhichPart == 1 && Input[i] != 'a') continue;
+            int start = 0, end = i;
+            if (WhichPart == 3)
+            {
+                start = Math.Max(0, i - limit);
+                end = Math.Min(line.Length, i + limit + 1);
+            }
+            pairs += line[start..end].ToCharArray().Count(c => c == line[i] - 32);
+        }
+        Output = pairs.ToString();
+    }
+}
