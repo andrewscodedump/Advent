59749ada9bea570af387d6e6a93505a2be6d43aa
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2025-11-13 00:40:27 +0000

    EC2025 Day08 done

---
+++ Everybody/2025/Done/Days06-10/Day08.cs
@@ -0,0 +1,69 @@
+﻿namespace Everybody2025;
+public class Day08 : Advent.Day
+{
+    public override void DoWork()
+    {
+
+        long[] order = InputNumbers[0];
+        int result = 0;
+        HashSet<(long first, long second)> chords = [];
+
+        switch (WhichPart)
+        {
+            case 1:
+                int number = TestMode ? 8 : 32;
+                for (int i = 0; i < order.Length - 1; i++)
+                {
+                    if (Math.Abs(order[i] - order[i + 1]) == number / 2) result++;
+                }
+                Output = result.ToString();
+                break;
+            case 2:
+                for (int i = 0; i < order.Length - 1; i++)
+                {
+                    (long, long) newString = (Math.Min(order[i], order[i + 1]), Math.Max(order[i], order[i + 1]));
+                    result += CountKnots(newString, chords);
+                    chords.Add(newString);
+                }
+                Output = result.ToString();
+                break;
+            case 3:
+                number = TestMode ? 8 : 256;    
+                for (int i = 0; i < order.Length - 1; i++)
+                {
+                    (long a, long b) = (Math.Min(order[i], order[i + 1]), Math.Max(order[i], order[i + 1]));
+                    chords.Add((a, b));
+                }
+                for (int i = 1; i <= number; i++)
+                    for (int j = i + 1; j <= number; j++)
+                        result = Math.Max(result, CountStrings((i, j), chords, true));
+                Output = result.ToString();
+                break;
+        }
+    }
+    private static bool Between (long a, (long c, long d) pair)
+    {
+        if (a == pair.c || a == pair.d) return false;
+        return a > pair.c && a < pair.d;
+    }
+
+    private static int CountKnots((long a, long b) test, HashSet<(long c, long d)> chords) => CountStrings(test, chords, false);
+
+    private static int CountStrings((long a, long b) test, HashSet<(long c, long d)> chords, bool includeSelf)
+    {
+        int result = 0;
+        foreach ((long c, long d) in chords)
+        {
+            if (includeSelf && ((test.a == c && test.b == d) || (test.b == c && test.a == d))) result++;
+            if (test.a == c || test.a == d || test.b == c || test.b == d) continue;
+            if (
+                (Between(test.a, (c, d)) && !Between(test.b, (c, d)))
+                || (!Between(test.a, (c, d)) && Between(test.b, (c, d)))
+                )
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
