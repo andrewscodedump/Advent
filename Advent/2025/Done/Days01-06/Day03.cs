5d7ee874c935a6b031f3016d9fb34917d01cc539
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2025-12-03 11:18:27 +0000

    AoC2025 Day03 done

---
+++ Advent/2025/Done/Days01-06/Day03.cs
@@ -0,0 +1,24 @@
+﻿namespace Advent2025;
+
+public class Day03 : Advent.Day
+{
+    public override void DoWork()
+    {
+        long total = 0;
+        int numberToSelect = WhichPart == 1 ? 2 : 12;
+        foreach (string bank in Inputs)
+        {
+            List<int> bankValues = [.. bank.ToCharArray().Select(x => x - 48)];
+            for (int i = numberToSelect - 1; i >= 0; i--)
+            {
+                //find the biggest that isn't in the last i
+                (long joltage, long pos) = bankValues[..^i].Select((val, pos) => (val, pos)).First(v => v.val == bankValues[..^i].Max());
+                total += joltage * (long)Math.Pow(10, i);
+                //remove the ones before the one we've just used
+                bankValues = bankValues[(int)(pos + 1)..];
+            }
+        }
+
+        Output = total.ToString();
+    }
+}
