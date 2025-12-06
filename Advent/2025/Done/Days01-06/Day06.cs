8461a6de971abe11bd2c9261ae0c55a4d5d08812
Andrew Watson <75695144+andrewscodedump@users.noreply.github.com>
2025-12-06 10:16:47 +0000

    AoC2025 Day06 refactoring

--- Advent/2025/Done/Days01-06/Day06.cs
+++ Advent/2025/Done/Days01-06/Day06.cs
@@ -6,23 +6,18 @@ public class Day06 : Advent.Day
     {
         long total = 0;
 
-        if (WhichPart == 1)
-        {
-            string[] operators = [.. Inputs[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries)];
-            for (int i = 0; i < operators.Length; i++)
-                total += DoSum(InputNumbers.Select(n => n[i]), operators[i][0]);
-        }
+        if (WhichPart == 1) total = Inputs[^1].Where(c => c != ' ').Select((op, i) => DoSum(InputNumbers.Select(n => n[i]), op)).Sum();
+
         else
         {
             List<long> numbers = [];
             for (int c = Inputs[0].Length - 1; c >= 0; c--)
             {
-                char[] column = [.. Inputs.Select(i => i[c])], digits = [.. column.Where(c => !" *+".Contains(c))];
-                if (digits.Length == 0) continue;
-                char op = column[^1];
-                numbers.Add(long.Parse(string.Join("", digits)));
-                if (op == ' ') continue;
-                total += DoSum(numbers, op);
+                char[] column = [.. Inputs.Select(i => i[c]).Where(v => v != ' ')];
+                if (column.Length == 0) continue;
+                numbers.Add(long.Parse(string.Join("", column.Where(c => !IsOp(c)))));
+                if (!IsOp(column[^1])) continue;
+                total += DoSum(numbers, column[^1]);
                 numbers = [];
             }
         }
@@ -30,4 +25,5 @@ public class Day06 : Advent.Day
     }
 
     private static long DoSum(IEnumerable<long> input, char op) => op switch { '+' => input.Sum(), _ => input.Aggregate(1, (long r, long n) => r * n) };
+    private static bool IsOp(char input) => input == '+' || input == '*';
 }
